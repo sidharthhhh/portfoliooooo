928774689cb6705391184ca6711c62552af41491
sidharth <sidharth@smarden.in>
2025-11-01 16:18:33 +0530

    aws cicd test with amplify

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,7 +31,7 @@ export default function Home() {
             <div className='w-1/2  flex flex-col items-center self-center lg:w-full lg:text-center'>
               <AnimatedText text="Bringing Ideas to Life with MERN Stack and DevOps Solutions" className='!text-5xl !text-left xl:!text-4xl lg:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl ' />
               <p className='my-4 text-base font-medium md:text-sm sm:text-xs '> As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications.
-                Explore my latest projects and articles, showcasing my expertise in Devops, cloud and MERN.</p>
+                Explore my latest projects and articles, showcasing my expertise in Devops, cloud and MERN. cicd test</p>
               <div className='flex items-center self-start mt-2 lg:self-center'>
                 <Link href="/sid.pdf" target={"_blank"}
                   className='flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold
