32f1cd1c31171096f9c61d742d1b79ab56bc1a28
sidharth <sidharth@smarden.in>
2025-11-01 16:04:24 +0530

    Simplify next.config.js for Amplify

--- next.config.js
+++ next.config.js
@@ -1,20 +1,37 @@
+// /** @type {import('next').NextConfig} */
+
+// const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+// let assetPrefix = ''
+// let basePath = ''
+
+// if (isGithubActions) {
+//   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+//   assetPrefix = `/${repo}/`
+//   basePath = `/${repo}`
+// }
+
+// const nextConfig = {
+//   output: 'export',
+//   assetPrefix: assetPrefix,
+//   basePath: basePath,
+//   images: {
+//     unoptimized: true,
+//   },
+// }
+
+// module.exports = nextConfig
+
+
+// -----------------aws config---------------------
+
 /** @type {import('next').NextConfig} */
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = ''
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
-
 const nextConfig = {
-  output: 'export',
-  assetPrefix: assetPrefix,
-  basePath: basePath,
+  // This is the only line you needed from your config
+  output: 'export', 
+  
+  // This is also good to keep for static exports
   images: {
     unoptimized: true,
   },
